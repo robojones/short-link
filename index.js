3a993717dc56ea0cbdbd87e1e077ed662fbcd5be
robojones <jonathan.scholz-weil@t-online.de>
2017-02-27 20:11:41 +0100

    setup marko

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-const http = require('http')
+const express = require('express')
+const path = require('path')
+const app = express()
+const marko = require('express-marko')
 
-const pack = require('./package.json')
+app.set('views', 'views')
 
-const server = http.createServer((req, res) => {
+app.use(marko)
 
+// app.use((req, res, next) => {
+//   const views = path.resolve(app.get('views'))
+//
+//   res.render = (view, locals = {}, callback = res) => {
+//     const template = path.join(views, view)
+//     if(process.env.NODE_ENV === 'development') {
+//       delete require.cache[require.resolve(template)]
+//     }
+//     require(template).render(locals, callback)
+//   }
+//
+//   next()
+// })
+
+app.get('/', (req, res, next) => {
+  res.render('index', {
+    name: 'jonés',
+    link: 'http://asdf.de/huhn'
+  })
 })
 
-server.listen(pack.ports[0])
+app.use((req, res, next) => {
+  console.log(req.url)
+})
+
+app.get('/*', (req, res, next) => {
+  next()
+})
+
+app.use((req, res, next) => {
+  res.status(404)
+  res.send('404 NOT FOUND')
+})
+
+app.listen(+process.env.PORT)
